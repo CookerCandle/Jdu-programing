c76a588c8774dc8343436999d108cc035070d150
cooker <jamoliddingafurov3223@gmail.com>
2025-08-24 00:13:33 +0500

    login parol registratsiya

---
+++ PHP/2-dars/5-vazifa.php
@@ -0,0 +1,10 @@
+<?php
+    $talabaID = 2403882;
+    $talabaName = "Ali";
+
+    if ($talabaID == 2403882 && $talabaName == "Ali") {
+        echo "Siz muvofoqiyatli kirdingiz";
+    } else {
+        echo "Parol yoki login xato terilgan";
+    }
+?>
