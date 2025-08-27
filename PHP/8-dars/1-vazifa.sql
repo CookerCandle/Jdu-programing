e0f2fe76486c1ac69df7af09eceacf5ea6a76863
cooker <jamoliddingafurov3223@gmail.com>
2025-08-28 03:34:14 +0500

    BIGINT ga o'zgartirildi (foydalanuvchilar bazasi)

--- PHP/8-dars/1-vazifa.sql
+++ PHP/8-dars/1-vazifa.sql
@@ -6,6 +6,6 @@ CREATE TABLE foydalanuvchilar (
     id INT AUTO_INCREMENT PRIMARY KEY,
     ism VARCHAR(25) NOT NULL,
     familiya VARCHAR(25) NOT NULL,
-    telefon_raqam INT,
+    telefon_raqam BIGINT(13),
     viloyat VARCHAR(35)
 );
