551425423905e2902ff1a8ca4d21590c590681a2
cooker <jamoliddingafurov3223@gmail.com>
2025-10-16 23:41:17 +0500

    added new column "soni"

--- SQL/3-dars/2-vazifa.sql
+++ SQL/3-dars/2-vazifa.sql
@@ -4,5 +4,6 @@ CREATE TABLE mahsulotlar (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nomi VARCHAR(100) NOT NULL,
     narxi DECIMAL(10, 2) NOT NULL,
+    soni INT DEFAULT 0,
     kategoriya VARCHAR(25)
 );
--- SQL/4-dars/1-vazifa.sql
+++ SQL/4-dars/1-vazifa.sql
@@ -1,8 +1,8 @@
 USE amazon;
 
-INSERT INTO mahsulotlar (nomi, narxi, kategoriya) VALUES
-('Laptop', 1200.00, 'Electronics'),
-('Smartphone', 800.00, 'Electronics'),
-('Desk Chair', 150.00, 'Furniture'),
-('Notebook', 5.00, 'Stationery'),
-('Pen', 2.00, 'Stationery');
+INSERT INTO mahsulotlar (nomi, narxi, soni, kategoriya) VALUES
+('Laptop', 1200.00, 10, 'Electronics'),
+('Smartphone', 800.00, 25, 'Electronics'),
+('Desk Chair', 150.00, 15, 'Furniture'),
+('Notebook', 5.00, 100, 'Stationery'),
+('Pen', 2.00, 200, 'Stationery');
