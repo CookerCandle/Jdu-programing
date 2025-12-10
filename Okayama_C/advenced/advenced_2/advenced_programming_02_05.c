4a0aad84263b8c7015003d034b32eb20d726f9d4
Cooker Candle <jamoliddingafurov3223@gmail.com>
2025-12-10 15:42:35 +0500

    added ;

--- Okayama_C/advenced/advenced_2/advenced_programming_02_05.c
+++ Okayama_C/advenced/advenced_2/advenced_programming_02_05.c
@@ -8,7 +8,7 @@ int main(void) {
     scanf("%s", input_string);
 
     while (input_string[lenght] != '\0') {
-        lenght++
+        lenght++;
     }
 
     printf("Lenght of the string: %d\n", lenght);
