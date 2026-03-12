50ddd92221bcf020f177637454c3d50941393cc9
shiro <siroj20011015@gmail.com>
2026-03-12 10:31:36 +0500

    4-topshiriq

--- laravel/resources/views/components/layouts/base.blade.php
+++ laravel/resources/views/components/layouts/base.blade.php
@@ -4,17 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>laravel</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="{{ route('hello') }}">Hello</a></li>
-            <li><a href="{{ route('about') }}">About</a></li>
-            <li><a href="{{ route('contact') }}">Contact</a></li>
-            <li><a href="{{ route('services') }}">Services</a></li>
-            <li><a href="{{ route('blog') }}">Blog</a></li>
-        </ul>
-    </nav>
+    <header>
+        <x-navbar></x-navbar>
+    <header>
 
     <main>
         {{ $slot }}
