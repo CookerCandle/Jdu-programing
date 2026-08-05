e9abdbdc63309e1caf4e55b1d8ddbc72e23398a0
CookerCandle <jamoliddingafurov3223@gmail.com>
2026-08-05 11:22:40 +0500

    day 13 modules

---
+++ files/day13_2_vazifa/fizika.py
@@ -0,0 +1,10 @@
+def tezlik(masofa, vaqt):
+    return masofa / vaqt
+
+
+def masofa(tezlik, vaqt):
+    return tezlik * vaqt
+
+
+def kuchlanish(tezlik, vaqt):
+    return tezlik / vaqt
---
+++ files/day13_2_vazifa/matematika.py
@@ -0,0 +1,14 @@
+def qoshih(a, b):
+    return a + b
+
+def ayirma(a, b):
+    return a - b
+
+def kopaytirish(a, b):
+    return a * b
+
+def bolish(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Nolga bo'lish mumkin emas"
